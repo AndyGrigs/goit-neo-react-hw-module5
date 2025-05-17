a1f45c7f92e24811cf4739b78f945b26bc11d5eb
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2025-05-18 00:43:38 +0200

    header styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,23 @@
 import { FilmIcon } from 'lucide-react';
 import { NavLink } from 'react-router-dom';
-import st from './Navbar.module.css';
+import styles from './Navbar.module.css';
 
 const Navbar = () => {
     return (
-        <nav>
-            <div>
+        <nav className={styles.navigation}>
+            <div className={styles.box}>
                 <div className="logo">
                     <FilmIcon size={24} />
                 </div>
                 <ul className="navlinks">
                     <NavLink
-                        className={({ isActive }) => (isActive ? st.activeLink : st.link)}
+                        className={({ isActive }) => (isActive ? styles.activeLink : styles.link)}
                         to="/"
                     >
                         Home
                     </NavLink>
                     <NavLink
-                        className={({ isActive }) => (isActive ? st.activeLink : st.link)}
+                        className={({ isActive }) => (isActive ? styles.activeLink : styles.link)}
                         to="/movies"
                     >
                         Movies
